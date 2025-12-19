78f679fc5754efcb4d78e3ba14e6d19250abe95b
Isaac Rios <jirb1213@live.com.mx>
2025-12-19 17:54:34 -0600

    App files clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-100">
-      <h1 className="text-6xl font-extrabold text-white bg-red-600 p-10 rounded-xl shadow-2xl">
-        Tailwind is WORKING!
-      </h1>
+    <div className="flex flex-col h-screen">
+      <Header />
     </div>
   );
 }
