fd3fe3f0f4c4ec2b20d8ef040e74f61fedab69b1
Isaac Rios <jirb1213@live.com.mx>
2025-12-19 17:53:43 -0600

    adds CSS variable token colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        background: "rgb(var(--background) / <alpha-value>)",
+        card: "rgb(var(--card) / <alpha-value>)",
+        primary: "rgb(var(--primary) / <alpha-value>)",
+        secondary: "rgb(var(--secondary) / <alpha-value>)",
+        green: "rgb(var(--green) / <alpha-value>)",
+        yellow: "rgb(var(--yellow) / <alpha-value>)",
+        red: "rgb(var(--red) / <alpha-value>)",
+      }
+    },
   },
   plugins: [],
 }
