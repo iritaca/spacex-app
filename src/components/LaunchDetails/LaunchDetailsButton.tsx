6227ea83c7b6bb98ed1c9c7b5f3292cd708986f2
Isaac Rios <jirb1213@live.com.mx>
2025-12-23 16:04:33 -0600

    LaunchDetails mobile button

---
+++ src/components/LaunchDetails/LaunchDetailsButton.tsx
@@ -0,0 +1,51 @@
+import Button from "../Button/Button";
+import ChevronIcon from "../icons/ChevronIcon";
+import { Skeleton } from "../Loader/Loader";
+
+interface LaunchDetailsButtonProps {
+  onClose: () => void;
+  mission?: string;
+  isLoading: boolean;
+}
+
+/**
+ * LaunchDetailsButton
+ *
+ * Displays the main title of the launch details view
+ *
+ *
+ * @param onClose - Callback fired when the button is clicked (Mobile behavior)
+ * @param mission - Mission name to display as the title
+ * @param isLoading - When true, render a loader skeleton
+ *
+ * @returns
+ */
+const LaunchDetailsButton = ({
+  onClose,
+  mission,
+  isLoading,
+}: LaunchDetailsButtonProps) => {
+  if (isLoading) return <Skeleton className="h-8" />;
+
+  // @Isaac - i need to implement isMobile to return a different component
+  return (
+    <Button
+      variant="custom"
+      onClick={onClose}
+      label={
+        <div className="flex flex-col gap-1 text-left">
+          <h3 className="flex gap-2">
+            <ChevronIcon
+              direction="left"
+              className="shrink-0 w-[28px] h-[28px] mt-[2px]"
+            />
+
+            <p className="font-bold text-2xl">{mission || "Mission Name"}</p>
+          </h3>
+        </div>
+      }
+    />
+  );
+};
+
+export default LaunchDetailsButton;
