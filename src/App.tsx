18c1161246a7804445954dd66625d29e2d1aec83
Isaac Rios <jirb1213@live.com.mx>
2025-12-22 21:59:20 -0600

    App root component and constants.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import MainBody from "./components/Main/MainBody/MainBody";
 
 function App() {
   return (
     <div className="flex flex-col h-screen">
       <Header />
+      <MainBody />
     </div>
   );
 }
---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const API_LAUNCHES_URL = "https://api.spacexdata.com/v5/launches";
+export const API_BASE_URL = "https://api.spacexdata.com/v4/";
+
+export const MOBILE_LAYOUT_WIDTH = 768;
