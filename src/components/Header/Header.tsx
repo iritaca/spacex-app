bb419dbb0bf3f41392d3d3f4ff04595b5b2f0b44
Isaac Rios <jirb1213@live.com.mx>
2025-12-22 22:01:06 -0600

    component: header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,16 @@
-import InfoIcon from "../icons/InfoIcon";
 import Logo from "../Logo/Logo";
+import InfoIconAndModal from "../InfoModal/InfoModal";
 
+/**
+ * App header containing primary branding and an info icon component
+ *
+ */
 const Header = () => {
   return (
     <header className="flex justify-between pt-8 px-4">
       <Logo />
-      <InfoIcon size="lg" />
+
+      <InfoIconAndModal />
     </header>
   );
 };
