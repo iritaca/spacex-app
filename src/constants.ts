732811e4f1a6a27c43a76547a22f7b28ca7ac3a4
Isaac Rios <jirb1213@live.com.mx>
2025-12-23 23:57:22 -0600

    constants: adds disabled styles

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const API_LAUNCHES_URL = "https://api.spacexdata.com/v5/launches";
+
 export const API_BASE_URL = "https://api.spacexdata.com/v4/";
 
 export const MOBILE_LAYOUT_WIDTH = 768;
@@ -6,3 +7,6 @@ export const MOBILE_LAYOUT_WIDTH = 768;
 export const MODAL_LABELED_BY = "app-info-title";
 
 export const API_ERROR_MESSAGE = "Failed to fetch launches";
+
+export const DISABLED_STYLES =
+  "opacity-30 cursor-not-allowed pointer-events-none select-none";
