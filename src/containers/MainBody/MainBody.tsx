e07cdbac99cc1096e780b5ea038287a3e885601a
Isaac Rios <jirb1213@live.com.mx>
2025-12-24 00:04:41 -0600

    MainBody: isolate fetchInitialData into a function, and replaces states by single status state for better control

--- src/containers/MainBody/MainBody.tsx
+++ src/containers/MainBody/MainBody.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
-import type { LaunchApi } from "../../types";
+import type { AppStatus, LaunchApi } from "../../types";
 
 import LaunchBrowser from "../LaunchBrowser/LaunchBrowser";
 import LaunchDetailsLayout from "../LaunchDetailsView/LaunchDetails";
 import { API_ERROR_MESSAGE, API_LAUNCHES_URL } from "../../constants";
 import LaunchBrowserHeader from "../../components/LaunchBrowser/LaunchBrowserHeader";
+import RetryButton from "../../components/RetryButton/RetryButton";
 
 /**
  * MainBody component -
@@ -23,46 +24,51 @@ import LaunchBrowserHeader from "../../components/LaunchBrowser/LaunchBrowserHea
  */
 const MainBody = () => {
   const [data, setData] = useState<LaunchApi[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [apiError, setApiError] = useState<string | null>(null);
+  const [status, setStatus] = useState<AppStatus>("loading");
   const [selectedMissionId, setSelectedMissionId] = useState<string | null>(
     null
   );
 
+  const fetchInitialData = async () => {
+    setStatus("loading");
+
+    try {
+      const res = await fetch(API_LAUNCHES_URL);
+      if (!res.ok) throw new Error(API_ERROR_MESSAGE);
+
+      const data = await res.json();
+      setData(data);
+      setStatus("success");
+    } catch (e) {
+      setStatus("error");
+    }
+  };
+
   useEffect(() => {
-    fetch(API_LAUNCHES_URL)
-      .then((res) => res.json())
-      .then((json) => {
-        setData(json);
-      })
-      .catch((err) => {
-        setApiError(API_ERROR_MESSAGE);
-        console.error(API_ERROR_MESSAGE, err);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+    fetchInitialData();
   }, []);
 
   return (
     <>
-      <LaunchBrowserHeader data={data} />
-      <main className="overflow-y-auto min-h-0 md:flex md:flex-row">
+      <LaunchBrowserHeader data={data} isLoading={status === "loading"} />
+      <main className="overflow-y-auto h-full min-h-0 md:flex md:flex-row">
         <LaunchBrowser
           data={data}
           setSelectedMissionId={setSelectedMissionId}
           selectedMissionId={selectedMissionId}
-          hasError={!!apiError}
-          // isLoading={isLoading} // @Isaac- I'll need to solve this later
+          isLoading={status === "loading"}
         />
 
         <LaunchDetailsLayout
           selectedMissionId={selectedMissionId}
           onClose={() => setSelectedMissionId(null)}
           data={data}
-          // isLoading={isLoading}
+          isLoading={status === "loading"}
         />
       </main>
+
+      {/* Visible when initial API load fails */}
+      {status === "error" && <RetryButton onClick={fetchInitialData} />}
     </>
   );
 };
